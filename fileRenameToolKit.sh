b3e10da380c8cb1b5307df6ef9902777b05a9a5f
Harman Dhillon <harmandhi22@gmail.com>
2019-02-19 16:45:24 -0500

    updated version

--- fileRenameToolKit.sh
+++ fileRenameToolKit.sh
@@ -4,30 +4,46 @@
 
 #global final variable
 declare -r SCRIPTNAME="fileRenameToolKit.sh"
-declare -r CURRENTFILEPATH=find . -name $SCRIPTNAME #BUG
-
-echo "$CURRENTFILEPATH"
+declare -r CURRENTFILEPATH=$(readlink -m "$SCRIPTNAME")
+declare -r CURRENTDIRECTORY=$(pwd)
 
 renameFileInFolder(){ #set this up this to use a path
 
-  read -p "Enter the first few letters of the file or file extension to be rename: " oldFileName
+  read -p "Enter the first few letters of the file or folder: " oldFileName
   read -p "Enter new file name: " newFileName
   read -p "Append date at the end of file name ( y/n ): " requireData
+  read -p "Place files in a new folder ( y/n ): " newFolder
 
   local fileCounter=1
   local currentDate=""
 
   #$requireData = "$requireData" | awk '{print tolower($0)}' #BUG
-  if [[ "$requireData" == "y" || "$requireData" == "Y" ]]
+  if [[ "$requireData" == "y" || "$requireData" == "Y" ]];
     then
       currentDate=`date +%Y-%m-%d`
    fi
+   
+   if [[ "$newFolder" == "y" || "$newFolder" == "Y" ]];
+	then
+		echo $CURRENTDIRECTORY
+		local newFileNameTemp="$newFileName$currentDate--$fileCounter"
+		mv $file "$newFileNameTemp"
+		mkdir  "$newFileName"
+		#mv "$newFileNameTemp" "$newFileName"
+		#(fileCounter++))
+		#else
+		#mv $file "$newFileName$currentDate--$fileCounter"
+		#((fileCounter++))
+	fi
+	
+	echo "old code"
 
-  for file in $(find . -name "$oldFileName*") #BUG maybe use the string that contains instead of starting chars
+  for file in $(find . -name "$oldFileName*")
     do
       if [[ "$file" != *$SCRIPTNAME* ]]; #not renaming this file itself
       then
-            mv $file "$newFileName$currentDate--$fileCounter"
+            local newFileNameTemp="$newFileName$currentDate--$fileCounter"
+		mv $file "$newFileNameTemp"
             ((fileCounter++))
       fi
   done
@@ -37,16 +53,28 @@ fileLocation(){
 
   echo "Enter path of the folder: "
   echo "Enter here if the location is the current directory"
+  
+  find -name "*c*" -type d
   read -p "   Choice: " folderPath
+}
+
+fileLocationBasedOfName(){
+  read -p "	Enter folder name: " folderName
+  local filePath=$(find -name "*$folderName*" -type d)
+  echo "Is this the correct file path $filePath"
+  read -p "( y/n ): " userResponse
+  if [[ "$userResponse" == "y" || "$userResponse" == "Y" ]]
+    then
+        find -name "*$folderName*" -type d
+   fi
+
 }
 
 userMenu(){
   echo "Select A Number Below."
-  echo "  1. Rename all files in current folder."
-  echo "  2. Rename all files in another folder."
-  echo "  3. Rename all files from a folder and place in another folder."
-  echo "  4. Find folder and rename files inside it."
-  echo "  5. Cancel." #BUG fix this
+  echo "  1. Rename all files/folders." #BUG fix this, allow picking of one
+  echo "  2. Find folder and rename files inside it."
+  echo "  4. Cancel." #BUG fix this
 }
 
 program(){
@@ -59,7 +87,7 @@ program(){
       renameFileInFolder
       ;;
   2)
-      echo "This feature doesn't yet exist"
+      fileLocationBasedOfName
       ;;
   3)
       echo "This feature doesn't yet exist"
