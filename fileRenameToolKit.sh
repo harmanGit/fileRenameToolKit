22e74b246bb73c59e063da9899bdbb471ad0bbc4
Harman Dhillon <harmandhi22@gmail.com>
2019-02-20 00:56:12 -0500

    "everything" is functional
    Need to clean up code, add comments, personal test, have others users test it.

--- fileRenameToolKit.sh
+++ fileRenameToolKit.sh
@@ -9,91 +9,114 @@ declare -r CURRENTDIRECTORY=$(pwd)
 
 fileCounter=1
 currentDate=""
-oldFileName=""
-newFileName=""
+oldObjectName=""
+newObjectName=""
 requireNewFolder=false
 
 userInput(){
-  read -p "Enter a few common letters in the $1 to be renamed: " oldFileNameTemp
-  read -p "Enter new name for the $1: " newFileNameTemp
+  read -p "Enter a few common letters in the $1 to be renamed: " oldObjectNameTemp
+  read -p "Enter new name for the $1s: " newObjectNameTemp
   read -p "Auto append date at the end ( y/n ): " requireDate
   read -p "Place $1s in a new folder ( y/n ): " requireNewFolderTemp
 
-  oldFileName="$oldFileNameTemp"
-  newFileName="$newFileNameTemp"
-  
+  oldObjectName="$oldObjectNameTemp"
+  newObjectName="$newObjectNameTemp"
+
   if [[ "$requireDate" == "y" || "$requireDate" == "Y" ]]; then
       currentDate=`date +%Y-%m-%d`
   fi
 
   if [[ "$requireNewFolderTemp" == "y" || "$requireNewFolderTemp" == "Y" ]]; then
       requireNewFolder=true
-      mkdir "$newFileName"
+      mkdir "$newObjectName"
   fi
 }
 
 renamer(){
-	local newFileNameTemp="$newFileName$currentDate--$fileCounter"
+	local newObjectNameTemp="$newObjectName$currentDate--$fileCounter"
 
 	if $requireNewFolder ; then
-		mv $1 "$newFileNameTemp"
-		mv "$newFileNameTemp" "$newFileName"
+		mv $1 "$newObjectNameTemp"
+		mv "$newObjectNameTemp" "$newObjectName"
 		((fileCounter++))
 	else
-		mv $1 "$newFileNameTemp"
+		mv $1 "$newObjectNameTemp"
 		((fileCounter++))
 	fi
 }
 
 renameFiles(){
-userInput "file"
+  userInput "file"
 
-  for file in $(find . -name "$oldFileName*")
+  for file in $(find . -name "$oldObjectName*")
     do
-	#not renaming this file itself
+	     #not renaming this file itself
       if [[ "$file" != *$SCRIPTNAME* && -f "$file" ]]; then
-	renamer "$file"
+	       renamer "$file"
       fi
   done
 }
 
 renameFolders(){
-userInput "folder"
+  userInput "folder"
 
-for file in $(find . -name "$oldFileName*")
-    do
-      if [[ -d "$file" ]]; then
-	renamer "$file"
-      fi
+  for file in $(find . -name "$oldObjectName*")
+      do
+        if [[ -d "$file" ]]; then
+	         renamer "$file"
+         fi
   done
 }
 
-fileLocation(){
-
-  echo "Enter path of the folder: "
-  echo "Enter here if the location is the current directory"
-  
-  find -name "*c*" -type d
-  read -p "   Choice: " folderPath
+objectsNotRenamed(){
+  if [[ $fileCounter -eq 1 ]];then
+    echo ""
+    echo "No files or folders renamed. Make sure the common letters match the"
+    echo "files or folders names. Also make sure the $SCRIPTNAME script is in"
+    echo "folder with your files or folders to be renamed, or use option 3"
+    echo "and give a file path to your files and folders to renamed."
+  fi
 }
 
-fileLocationBasedOfName(){
-  read -p "	Enter folder name: " folderName
-  local filePath=$(find -name "*$folderName*" -type d)
-  echo "Is this the correct file path $filePath"
-  read -p "( y/n ): " userResponse
-  if [[ "$userResponse" == "y" || "$userResponse" == "Y" ]]
-    then
-        find -name "*$folderName*" -type d
-   fi
+pathRenamer(){
+  read -p "	Enter path to the folder where to rename files/folders: " filePath
 
+  cp $SCRIPTNAME $filePath
+
+  cd $filePath
+
+  echo "Select A Number Below."
+  echo "  1. Rename files."
+  echo "  2. Rename folders."
+  echo "  3. Cancel." #BUG fix this
+
+  read -p "Selection: " userPathOption
+  case "$userPathOption" in
+  1)
+      renameFiles
+      objectsNotRenamed
+      ;;
+  2)
+      renameFolders
+      objectsNotRenamed
+      ;;
+  3)
+      echo "Exiting Skynet Compute Host [UPDATE BLOCKCHAIN LEDGER] ..."
+      ;;
+  *)
+      echo "Invalid Choice, Pick again..."
+      program
+      ;;
+  esac
+
+  rm $SCRIPTNAME
 }
 
 userMenu(){
   echo "Select A Number Below."
   echo "  1. Rename files."
   echo "  2. Rename folders."
-  echo "  3. Find folder and rename files inside it."
+  echo "  3. Rename files/folders in given file path."
   echo "  4. Cancel." #BUG fix this
 }
 
@@ -105,12 +128,15 @@ program(){
   case "$userOption" in
   1)
       renameFiles
+      objectsNotRenamed
       ;;
   2)
       renameFolders
+      objectsNotRenamed
       ;;
   3)
-      echo "NOT COMPLETE YET, Pick again..."
+      pathRenamer
+      #objectsNotRenamed
       ;;
   4)
       echo "Exiting Skynet Compute Host [UPDATE BLOCKCHAIN LEDGER] ..."
